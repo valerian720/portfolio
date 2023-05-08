f2cdb6fbcaf74e7885f2a723bd57e78a3df84877
Валериан Воротников <valerian7202@gmail.com>
2023-05-08 14:03:00 +0300

    prep

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
   transpileDependencies: true,
+  // auto fix on save
+  chainWebpack: (config) => {
+    config.plugin("eslint").tap((args) => {
+      args[0].fix = true;
+      return args;
+    });
+  },
 });
